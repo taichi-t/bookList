205de08c599044f42ecf6cd9f13e32a97482e266
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-04 02:38:21 -0700

    Updates

---
+++ precache-manifest.a278cf4a9a1c79fe32454abde5c08e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea4d2f0971c38d87305fbe131d18f3c",
+    "url": "/booklist/index.html"
+  },
+  {
+    "revision": "16ecf9d9dc56a8f54b3b",
+    "url": "/booklist/static/css/main.a1a1e574.chunk.css"
+  },
+  {
+    "revision": "8cc9082b2501c5d1b3b2",
+    "url": "/booklist/static/js/2.a72f624d.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/booklist/static/js/2.a72f624d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ecf9d9dc56a8f54b3b",
+    "url": "/booklist/static/js/main.45a81823.chunk.js"
+  },
+  {
+    "revision": "d838f7bcad7af0ea09ed",
+    "url": "/booklist/static/js/runtime-main.3febf5ac.js"
+  }
+]);
